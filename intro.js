f6f71254317c185386ecb52d78de8c37c1870db2
you@christopherdominic <chriseze0@gmail.com>
2026-01-15 23:42:54 +0100

    techified

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Christopher Dominic Eze";
+const age = 30;
+const favouriteProgrammingLanguage = "Rust";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favourite Programming Language:", favouriteProgrammingLanguage);
